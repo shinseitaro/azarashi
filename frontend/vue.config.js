ebc14bfe51fbd47eb42887b1ecce3c4e5468f38b
Kazuki Imamura <kaizumaki@gmail.com>
2019-11-18 16:32:50 +0900

    Vuetifyをインストール

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  outputDir: '../static',
-  indexPath: '../templates/index.html',
+  outputDir: "../static",
+  indexPath: "../templates/index.html",
   publicPath: process.env.NODE_ENV === 'production'
     ? '/static/'
-    : '/'
-}
+    : '/',
+  transpileDependencies: ["vuetify"]
+};
