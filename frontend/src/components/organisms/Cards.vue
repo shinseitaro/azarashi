bc2162c021f0263e72c98e16db8fc3b4fadf7bfa
Kazuki Imamura <kaizumaki@gmail.com>
2019-11-19 17:30:38 +0900

    Vuetifyサンプル修正

--- frontend/src/components/organisms/Cards.vue
+++ frontend/src/components/organisms/Cards.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="grey lighten-5">
-    <v-row v-for="n in 2" :key="n" :class="n === 1 ? 'mb-6' : ''">
-      <v-col v-for="k in n + 1" :key="k">
+    <v-row>
+      <v-col v-for="n in 8" :key="n" cols="12" md="3">
         <Card />
       </v-col>
     </v-row>
