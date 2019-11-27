8bbbfc8d1cda7ef087dbfbc8c2cd5dac16368ac9
nrm176 <nerimplo.tokyo@gmail.com>
2019-11-27 11:48:41 +0900

    * 都道府県データ投入時のエラー回避のため下記のフィールドをblank=True, null=Trueにした infra > category dam > scale_bank_height, scale_bank_span, bank_volume, total_pondage, year_of_completion, positional_information_precision, prefecture
    * レコードがすでに存在する場合、都道府県フィールドをアップデートしたかったのでsetattrを使った。

--- infrastructure/models.py
+++ infrastructure/models.py
@@ -1,4 +1,4 @@
-import uuid 
+import uuid
 from django.utils import timezone
 from django.contrib.gis.db import models
 
@@ -20,28 +20,28 @@ class Infra(models.Model):
 	class Meta:
 		# マイグレーションするときにテーブルは作成されない設定
 		# よって他のモデルに継承するためのモデルになる
-		abstract = True 
-		
+		abstract = True
+
 
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	name = models.CharField(verbose_name='名称', max_length=50)
 	address = models.CharField(verbose_name='住所', max_length=60)
-	category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+	category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT, blank=True, null=True)
 	registered_at = models.DateTimeField(default=timezone.now)
 	modified_at = models.DateTimeField(blank=True, null=True)
 	url = models.URLField(blank=True)
 
-	geom = models.PointField(srid=4019) 
-	
+	geom = models.PointField(srid=4019)
+
 	# card はいったん保留
 	#card = models.ManyToManyField()
-	
+
 	def modified(self):
-		self.modified_at = timezone.now() 
+		self.modified_at = timezone.now()
 		self.save()
 
 	def __str__(self):
-		return self.name 
+		return self.name
 
 
 # class Card(models.Model):
