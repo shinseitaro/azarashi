4404c740e4e39648a60f5bedc504f54b3c2ec709
nrm176 <nerimplo.tokyo@gmail.com>
2019-12-02 15:58:27 +0900

    ダムのpurpose_codeやinstitution_in_chargeが意味のない数字になっていたので、Serializerを作ってnameを渡す様にしました。 "purpose_code": [{"name": "灌漑，特定(新規)灌漑用水"}] とdictになっているので、事実上フロント側で表示することを考えたらListにしていいかも。 参考url: https://stackoverflow.com/questions/33182092/django-rest-framework-serializing-many-to-many-field https://stackoverflow.com/questions/28200485/foreign-key-value-in-django-rest-framework

--- v1/serializers.py
+++ v1/serializers.py
@@ -1,11 +1,12 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from dam.models import Dam 
+from dam.models import Dam, Institution, Purpose
+
 
 class DamSerializer(serializers.ModelSerializer):
 
-	class Meta: 
-		model = Dam 
+	class Meta:
+		model = Dam
 		exclude = ["registered_at", "modified_at"]
 
 
@@ -14,11 +15,25 @@ class DamListSerializer(serializers.ListSerializer):
 	"""
 	child = DamSerializer()
 
-class DamGeoFeatureModelSerializer(GeoFeatureModelSerializer):
+class InstitutionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Institution
+        fields = ('name',)
 
-	class Meta: 
-		model = Dam 
-		geo_field = "geom"
-		id_field = False
-		exclude = ("registered_at", "modified_at", "id" )
-		#fields = ("name", "address", "river_name")
+class PurposeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purpose
+        fields = ('name',)
+
+
+class DamGeoFeatureModelSerializer(GeoFeatureModelSerializer):
+    type_code = serializers.CharField(source='type_code.description')
+    institution_in_charge = InstitutionSerializer(read_only=True, many=True)
+    purpose_code = PurposeSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Dam
+        geo_field = "geom"
+        id_field = False
+        exclude = ("registered_at", "modified_at", "id")
+    # fields = ("name", "address", "river_name")
