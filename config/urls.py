3a68b814120d605ec6dedb1016b221c9ae3d7fc4
Kazuki Imamura <kaizumaki@gmail.com>
2019-12-03 17:01:14 +0900

    DRFでファイルアップロードのサンプル作成（WIP）

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 #from django.contrib import admin
 from django.contrib.gis import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter 
+from rest_framework.routers import DefaultRouter
 
-from v1.views import DamViewSet 
+from v1.views import DamViewSet
 
 router = DefaultRouter()
 router.register('dam', DamViewSet)
@@ -13,4 +13,5 @@ router.register('dam', DamViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('card/', include('card.urls')),
 ]
