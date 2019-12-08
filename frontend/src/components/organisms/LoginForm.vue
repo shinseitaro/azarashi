5ac4ce367a6b0e26bd9f62ff934e4fe8c4ef91ed
Kazuki Imamura <kaizumaki@gmail.com>
2019-12-09 01:13:30 +0900

    oauth2認証のサンプル（WIP）

---
+++ frontend/src/components/organisms/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-btn large color="primary" @click="login">Login</v-btn>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+import oauth from 'axios-oauth-client';
+
+export default {
+  methods: {
+    login: async function() {
+      const getAuthorizationCode = oauth.client(axios.create(), {
+        url: 'http://45.77.22.11:8000/auth/convert-token',
+        grant_type: 'convert_token',
+        client_id: '',
+        client_secret: '',
+        backend: 'github',
+        token: '',
+      });
+      const auth = await getAuthorizationCode();
+      console.log(auth);
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/pages/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Header />
+    <v-content>
+      <LoginForm />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Header from '../organisms/Header.vue';
+import LoginForm from '../organisms/LoginForm.vue';
+
+export default {
+  components: {
+    Header,
+    LoginForm,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
