c905fe43824c4718489cdcf5b9ae60aac549ccf1
Kazuki Imamura <kaizumaki@gmail.com>
2019-12-12 22:57:08 +0900

    LOGIN_REDIRECT_URLを設定

--- config/settings/base.py
+++ config/settings/base.py
@@ -152,6 +152,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
 )
 
+LOGIN_REDIRECT_URL = '/post'
+
 # Upload files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
