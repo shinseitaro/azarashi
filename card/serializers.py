2156a2f5bb1e11506484cfbf3b2bf4ab034b66d8
Kazuki Imamura <kaizumaki@gmail.com>
2019-12-17 16:00:15 +0900

    cardのviewとserializersを修正

--- card/serializers.py
+++ card/serializers.py
@@ -3,6 +3,10 @@ from .models import Card
 
 
 class CardSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Card
         fields = "__all__"
--- card/views.py
+++ card/views.py
@@ -1,20 +1,19 @@
+from rest_framework import viewsets
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework import status
 from datetime import datetime
 from .serializers import CardSerializer
 
 
-class FileUploadView(APIView):
+class FileUploadView(viewsets.ViewSet):
     parser_class = (FileUploadParser,)
 
-    def post(self, request, *args, **kwargs):
-
+    def create(self, request):
         card_serializer = CardSerializer(data=request.data)
 
         if card_serializer.is_valid():
-            card_serializer.save(user=self.request.user, published_date=datetime.now())
+            card_serializer.save(published_date=datetime.now())
             return Response(card_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(card_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(card_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
