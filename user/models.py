64540b07895cf959c430dc92254184fd4a37ba64
nrm176 <nerimplo.tokyo@gmail.com>
2019-12-18 14:23:49 +0900

    AUTH_USER_MODELがuser.Userの場合に使う様にした。

--- user/models.py
+++ user/models.py
@@ -1,48 +1,49 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+
+if settings.AUTH_USER_MODEL == 'user.User':
+    class UserManager(BaseUserManager):
+
+        def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+            if not email:
+                raise ValueError('Users must have an email address')
+            now = timezone.now()
+            email = self.normalize_email(email)
+            user = self.model(
+                email=email,
+                is_staff=is_staff,
+                is_active=True,
+                is_superuser=is_superuser,
+                last_login=now,
+                date_joined=now,
+                **extra_fields
+            )
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+
+        def create_user(self, email, password, **extra_fields):
+            return self._create_user(email, password, False, False, **extra_fields)
+
+        def create_superuser(self, email, password, **extra_fields):
+            user = self._create_user(email, password, True, True, **extra_fields)
+            user.save(using=self._db)
+            return user
 
 
-class UserManager(BaseUserManager):
+    class User(AbstractBaseUser, PermissionsMixin):
+        email = models.EmailField(max_length=254, unique=True)
+        name = models.CharField(max_length=254, null=True, blank=True)
+        is_staff = models.BooleanField(default=False)
+        is_superuser = models.BooleanField(default=False)
+        is_active = models.BooleanField(default=True)
+        last_login = models.DateTimeField(null=True, blank=True)
+        date_joined = models.DateTimeField(auto_now_add=True)
 
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-        now = timezone.now()
-        email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            is_staff=is_staff,
-            is_active=True,
-            is_superuser=is_superuser,
-            last_login=now,
-            date_joined=now,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+        USERNAME_FIELD = 'email'
+        EMAIL_FIELD = 'email'
+        REQUIRED_FIELDS = []
 
-    def create_user(self, email, password, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        user = self._create_user(email, password, True, True, **extra_fields)
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=254, unique=True)
-    name = models.CharField(max_length=254, null=True, blank=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    last_login = models.DateTimeField(null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
+        objects = UserManager()
