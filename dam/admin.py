409554e25dc28c4ff9c9212965338fd39918aeb1
nrm176 <nerimplo.tokyo@gmail.com>
2019-12-30 17:42:21 +0900

    不要なコードの削除

--- dam/admin.py
+++ dam/admin.py
@@ -1,4 +1,3 @@
-#from django.contrib import admin
 from django.contrib.gis import admin
 from dam.models import Dam
 from dam.models import DamCardDistributionPlace
@@ -7,8 +6,4 @@ from dam.models import DamType
 admin.site.register(Dam, admin.OSMGeoAdmin)
 admin.site.register(DamCardDistributionPlace, admin.OSMGeoAdmin)
 admin.site.register(DamType, admin.OSMGeoAdmin)
-# @admin.site.register(Dam)
-# class DamAdmin(admin.OSMGeoAdmin):
-#     model = Dam
-#     search_fields = ('name', )
 
