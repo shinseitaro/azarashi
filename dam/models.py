20131b4ea7da1bd3dbd7cbe9050cc65e536cf4c5
nrm176 <nerimplo.tokyo@gmail.com>
2019-12-30 14:07:12 +0900

    ダムカード配布所モデルの修正とデータセット

--- dam/models.py
+++ dam/models.py
@@ -38,21 +38,6 @@ class Purpose(models.Model):
         purpose = cls(id=id, name=name)
         return purpose
 
-class DamCardDistributionPlace(models.Model):
-    id = models.CharField(max_length=4, primary_key=True)
-    name = models.CharField(max_length=50)
-    url = models.URLField()
-    address = models.CharField(max_length=100)
-    operation_hour = models.CharField(max_length=200)
-    prefecture = models.CharField(max_length=4)
-
-    def __str__(self):
-        return self.name
-
-    @classmethod
-    def create(cls, id, name, url, address, operation_hour, prefecture):
-        return cls(id=id, name=name, url=url, address=address, operation_hour=operation_hour, prefecture=prefecture)
-
 class Dam(Infra):
     dam_code = models.IntegerField(null=False)
     water_system_name = models.CharField(max_length=50, blank=True, null=True)
@@ -71,3 +56,21 @@ class Dam(Infra):
 
     def __str__(self):
         return '{}'.format(self.name)
+
+class DamCardDistributionPlace(models.Model):
+    id = models.CharField(max_length=4, primary_key=True)
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    address = models.CharField(max_length=100)
+    operation_hour = models.CharField(max_length=200)
+    prefecture = models.CharField(max_length=4)
+    dam_id = models.ManyToManyField(Dam, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def create(cls, id, name, url, address, operation_hour, prefecture, dam_id):
+        return cls(id=id, name=name, url=url, address=address,
+                   operation_hour=operation_hour, prefecture=prefecture,
+                   dam_id=dam_id)
