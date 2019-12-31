8572805cfbae9cb8c8720e35b4ae52391861e45e
Kazuki Imamura <kaizumaki@gmail.com>
2020-01-01 01:50:35 +0900

    カードのスタイルを調整

--- frontend/src/components/molecules/CardItem.vue
+++ frontend/src/components/molecules/CardItem.vue
@@ -1,53 +1,59 @@
 <template>
-  <v-card class="mx-auto" max-width="374">
-    <v-img :aspect-ratio="88 / 63" src="/img/no_cards_posted.jpg"></v-img>
+  <v-card class="mx-auto card" max-width="374" min-height="100%">
+    <v-row class="card-inner" justify="space-between" no-gutters>
+      <v-col>
+        <v-img :aspect-ratio="88 / 63" src="/img/no_cards_posted.jpg"></v-img>
 
-    <v-card-title>
-      <v-row align="center" justify="space-between" class="mx-0">
-        <span>{{ item.name }}</span>
-        <v-checkbox
-          v-model="checkbox"
-          off-icon="mdi-heart"
-          on-icon="mdi-heart"
-          color="red"
-          hide-details
-          class="mt-0"
-        ></v-checkbox>
-      </v-row>
-    </v-card-title>
+        <v-card-title>
+          <v-row align="center" justify="space-between" class="mx-0">
+            <span>{{ item.name }}</span>
+            <v-checkbox
+              v-model="checkbox"
+              off-icon="mdi-heart"
+              on-icon="mdi-heart"
+              color="red"
+              hide-details
+              class="mt-0"
+            ></v-checkbox>
+          </v-row>
+        </v-card-title>
 
-    <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
 
-        <div class="grey--text ml-4">4.5 (413)</div>
-      </v-row>
-      <v-row align="start" justify="space-between" class="mx-0 my-4">
-        <div>
-          <div class="subtitle-1 black--text">
-            {{ item.address }}
-          </div>
-          <div>{{ item.water_system_name }}水系 {{ item.river_name }}</div>
-        </div>
-        <v-btn icon>
-          <v-icon>mdi-map-marker</v-icon>
-        </v-btn>
-      </v-row>
-    </v-card-text>
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+          <v-row align="start" justify="space-between" class="mx-0 my-4">
+            <div>
+              <div class="subtitle-1 black--text">
+                {{ item.address }}
+              </div>
+              <div>{{ item.water_system_name }}水系 {{ item.river_name }}</div>
+            </div>
+            <v-btn icon>
+              <v-icon>mdi-map-marker</v-icon>
+            </v-btn>
+          </v-row>
+        </v-card-text>
+      </v-col>
 
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn><v-icon left>mdi-map-legend</v-icon>配布所</v-btn>
-      <v-btn><v-icon left>mdi-cards</v-icon>投稿一覧</v-btn>
-      <v-btn><v-icon left>mdi-pencil-plus</v-icon>投稿する</v-btn>
-    </v-card-actions>
+      <v-col class="card-footer">
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn><v-icon left>mdi-map-legend</v-icon>配布所</v-btn>
+          <v-btn><v-icon left>mdi-cards</v-icon>投稿一覧</v-btn>
+          <v-btn><v-icon left>mdi-pencil-plus</v-icon>投稿する</v-btn>
+        </v-card-actions>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
@@ -66,3 +72,18 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.card {
+  display: flex;
+}
+
+.card-inner {
+  flex-direction: column;
+  flex: 1 0 100%;
+}
+
+.card-footer {
+  flex-grow: 0;
+}
+</style>
