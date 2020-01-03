386cffc18d6fa3911ed87f0b239c859a41379efa
Kazuki Imamura <kaizumaki@gmail.com>
2020-01-03 22:01:32 +0900

    検索フィールドを子コンポーネント化

---
+++ frontend/src/components/atoms/SearchTextField.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-text-field
+    outlined
+    background-color="white"
+    :label="label"
+    prepend-inner-icon="mdi-magnify"
+    @change="search"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  props: ['label', 'searchAction'],
+  methods: {
+    search: function(e) {
+      if (e !== undefined) {
+        this.$store
+          .dispatch('map/' + this.searchAction, e)
+          .then(() => {
+            this.$store.dispatch('map/emptySearchField');
+          })
+          .then(() => {
+            this.$store.dispatch('map/searchResult');
+          });
+      }
+    },
+  },
+};
+</script>
