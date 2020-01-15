d79d98859b1b3a1e76d2584735134fd4010e88fe
nrm176 <nerimplo.tokyo@gmail.com>
2020-01-15 23:26:20 +0900

    [WIP] リファクタリング

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,9 @@
-# from django.contrib import admin
 from django.contrib.gis import admin
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-from user.views import GithubLoginView, TwitterLoginView, FetchTokenView, UserJWTDetailView
+from user.views import UserJWTDetailView
 from rest_framework_jwt.views import obtain_jwt_token
 from user import urls as user_url
 from card.views import CardViewSet
@@ -25,17 +24,12 @@ router.register('dam', DamIdViewSet, )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/', include('rest_social_auth.urls_jwt_pair')),
-    path('api/login/', include('rest_social_auth.urls_token')),
-    path('api/login/', include('rest_social_auth.urls_session')),
     path('auth/', include('rest_framework_social_oauth2.urls')),
     path('api/', include(router.urls)),
     path('accounts/', include('allauth.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include(user_url)),
-    path('rest-auth/github/', GithubLoginView.as_view(), name='github_login'),
-    path('rest-auth/twitter/', TwitterLoginView.as_view(), name='twitter_login'),
     path('api-token-auth/', obtain_jwt_token),
-    path('token/', FetchTokenView.as_view(), name='token'),
     path('api/user/jwt/', UserJWTDetailView.as_view(), name="current_user_jwt"),
 
     re_path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT}),
