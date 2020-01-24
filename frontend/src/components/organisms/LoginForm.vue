dd662ea8f2450b457e991e28ec3a868f6a8af1ca
Kazuki Imamura <kaizumaki@gmail.com>
2020-01-24 09:13:31 +0900

    ログインフォームからemailフィールドを削除

--- frontend/src/components/organisms/LoginForm.vue
+++ frontend/src/components/organisms/LoginForm.vue
@@ -9,12 +9,6 @@
           :rules="[rules.required]"
         ></v-text-field>
         <v-text-field
-          v-model="email"
-          label="e-mail"
-          type="email"
-          :rules="[rules.required]"
-        ></v-text-field>
-        <v-text-field
           v-model="password"
           label="password"
           :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
@@ -70,7 +64,7 @@ export default {
   },
   computed: {
     disabledBtn: function() {
-      return this.username === '' || this.email === '' || this.password === '';
+      return this.username === '' || this.password === '';
     },
     ...mapState({
       error: state => state.auth.error,
@@ -81,7 +75,6 @@ export default {
       await this.$store.dispatch('auth/logout');
       const params = new URLSearchParams();
       params.append('name', this.username);
-      params.append('email', this.email);
       params.append('password', this.password);
       await this.$store.dispatch('auth/login', params);
       this.$store
