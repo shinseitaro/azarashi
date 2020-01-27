95bad581e816bf311fc4c2d03805012c45370b97
shinseitaro <shinsei.all@gmail.com>
2020-01-27 13:08:34 +0000

    nginx設定

--- config/settings/base.py
+++ config/settings/base.py
@@ -148,7 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # REST Framework
 REST_FRAMEWORK = {
