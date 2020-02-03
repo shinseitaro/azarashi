5714407af2cb898288b0ac961c28e5ad7243138a
root <root@vultr.guest>
2020-02-03 12:07:50 +0000

    settings for development

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,7 +7,7 @@ from .base import *
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -36,3 +36,67 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'filters': {
+       'require_debug_false': {
+           '()': 'django.utils.log.RequireDebugFalse',
+       },
+       'require_debug_true': {
+           '()': 'django.utils.log.RequireDebugTrue',
+       },
+   },
+   'formatters': {
+       'django.server': {
+           '()': 'django.utils.log.ServerFormatter',
+           'format': '[%(server_time)s] %(message)s a',
+       },
+       'verbose': {
+           'format': '%(levelname)s %(asctime)s %(module)s '
+                     '%(process)d %(thread)d %(message)s'
+       },
+   },
+   'handlers': {
+       'file': {  # どこに出すかの設定に名前をつける `file`という名前をつけている
+           'level': 'DEBUG',  # DEBUG以上のログを取り扱うという意味
+           'class': 'logging.FileHandler',  # ログを出力するためのクラスを指定
+           'filename': os.path.join(BASE_DIR, 'django.log'),  # どこに出すか
+           'formatter': 'verbose',  # どの出力フォーマットで出すかを名前で指定
+       },
+       'console': {
+           'level': 'INFO',
+           'filters': ['require_debug_true'],
+           'class': 'logging.StreamHandler',
+           'formatter': 'verbose',
+       },
+       'django.server': {
+           'level': 'INFO',
+           'class': 'logging.StreamHandler',
+           'formatter': 'django.server',
+       },
+       'mail_admins': {
+           'level': 'ERROR',
+           'filters': ['require_debug_false'],
+           'class': 'django.utils.log.AdminEmailHandler'
+       }
+   },
+   'loggers': {
+       'django': {
+           'handlers': ['console', 'file', 'mail_admins'],
+           'level': 'INFO',
+       },
+       'django.server': {
+           'handlers': ['django.server'],
+           'level': 'INFO',
+           'propagate': False,
+       },
+       #追加
+       'azarashi': {
+           'handlers': ['console', 'file'],
+           'level': 'INFO',
+           'propagate': False,
+       },
+   }
+}
